0241ee759165dd1dd1902a252969885201aabb8a
DISK-C137 <csd26prime@gmail.com>
2024-08-10 22:25:49 +0700

    Update format

--- action.php
+++ action.php
@@ -1,79 +1,78 @@
 <?php
-    $connect = new PDO("mysql:host=localhost; dbname=vuecrud", "root", "");
-    $received_data = json_decode(file_get_contents("php://input"));
-    $data = array();
+$connect = new PDO("mysql:host=localhost; dbname=vuecrud", "root", "");
+$received_data = json_decode(file_get_contents("php://input"));
+$data = array();
 
-    if($received_data->action == "fetchall"){
-        $query = "SELECT * FROM tbl_users";
-        $statement = $connect->prepare($query);
-        $statement->execute();
-        while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-            $data[] = $row;
-        }
-        echo json_encode($data);
+if ($received_data->action == "fetchall") {
+    $query = "SELECT * FROM tbl_users";
+    $statement = $connect->prepare($query);
+    $statement->execute();
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $data[] = $row;
     }
+    echo json_encode($data);
+}
 
-    if($received_data->action == 'insert'){
-        $data = array(
-            ':first_name' => $received_data->firstName,
-            ':last_name' => $received_data->lastName,
-            ':email' => $received_data->email
-        );
+if ($received_data->action == 'insert') {
+    $data = array(
+        ':first_name' => $received_data->firstName,
+        ':last_name' => $received_data->lastName,
+        ':email' => $received_data->email
+    );
 
-        $query = "INSERT INTO tbl_users (first_name, last_name, email) VALUES (:first_name, :last_name, :email)";
+    $query = "INSERT INTO tbl_users (first_name, last_name, email) VALUES (:first_name, :last_name, :email)";
 
-        $statement = $connect->prepare($query);
-        $statement->execute($data);
-        $output = array(
-            'message' => 'Data Inserted'
-        );
-        echo json_encode($output);
+    $statement = $connect->prepare($query);
+    $statement->execute($data);
+    $output = array(
+        'message' => 'Data Inserted'
+    );
+    echo json_encode($output);
+}
+
+if ($received_data->action == 'fetchSingle') {
+    $query = "SELECT * FROM tbl_users WHERE id = '" . $received_data->id . "'";
+    $statement = $connect->prepare($query);
+    $statement->execute();
+    $result = $statement->fetchAll();
+
+    foreach ($result as $row) {
+        $data['id'] = $row['id'];
+        $data['first_name'] = $row['first_name'];
+        $data['last_name'] = $row['last_name'];
+        $data['email'] = $row['email'];
     }
+    echo json_encode($data);
+}
 
-    if($received_data->action == 'fetchSingle'){
-        $query = "SELECT * FROM tbl_users WHERE id = '".$received_data->id."'";
-        $statement = $connect->prepare($query);
-        $statement->execute();
-        $result = $statement->fetchAll();
+if ($received_data->action == 'update') {
+    $data = array(
+        ':first_name' => $received_data->firstName,
+        ':last_name' => $received_data->lastName,
+        ':email' => $received_data->email,
+        ':id' => $received_data->hiddenId
+    );
 
-        foreach($result as $row) {
-            $data['id'] = $row['id'];
-            $data['first_name'] = $row['first_name'];
-            $data['last_name'] = $row['last_name'];
-            $data['email'] = $row['email'];
-        }
-        echo json_encode($data);   
-    }
+    $query = "UPDATE tbl_users SET first_name = :first_name, last_name = :last_name, email = :email WHERE id = :id";
 
-    if($received_data->action == 'update'){
-        $data = array(
-            ':first_name' => $received_data->firstName,
-            ':last_name' => $received_data->lastName,
-            ':email' => $received_data->email,
-            ':id' => $received_data->hiddenId
-        );
+    $statement = $connect->prepare($query);
+    $statement->execute($data);
+    $output = array(
+        'message' => 'Data update'
+    );
 
-        $query = "UPDATE tbl_users SET first_name = :first_name, last_name = :last_name, email = :email WHERE id = :id";
+    echo json_encode($output);
+}
 
-        $statement = $connect->prepare($query);
-        $statement->execute($data);
-        $output = array(
-            'message' => 'Data update'
-        );
+if ($received_data->action == 'delete') {
+    $query = "DELETE FROM tbl_users WHERE id = '" . $received_data->id . "'";
 
-        echo json_encode($output); 
-    }
+    $statement = $connect->prepare($query);
+    $statement->execute();
 
-    if($received_data->action == 'delete'){
-        $query = "DELETE FROM tbl_users WHERE id = '".$received_data->id."'";
+    $output = array(
+        'message' => 'Data deleted'
+    );
 
-        $statement = $connect->prepare($query);
-        $statement->execute();
-
-        $output = array(
-            'message' => 'Data deleted'
-        );
-        
-        echo json_encode($output);   
-    }
-?>
+    echo json_encode($output);
+}
